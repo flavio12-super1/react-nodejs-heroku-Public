f46bf02b42984ee41d05eb628d10f77f104bfb7e
flavio herrera <herreraflavio0@gmail.com>
2022-09-11 23:24:36 -0700

    changed profile

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  function getInfo() {
+    axios
+      .post("http://localhost:8000/getInfo", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        alert(res.data);
+      });
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        Learn React
+        <button onClick={getInfo}>get info</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = require("express")();
+
+const path = require("path");
+const cors = require("cors");
+const http = require("http").createServer(app);
+
+const normalizePort = (port) => parseInt(port, 10);
+const PORT = normalizePort(process.env.PORT || 8000);
+
+app.use(express.urlencoded({ extended: false }));
+
+//cors
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+//resolve path for front end
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
+
+app.post("/getInfo", (req, res) => {
+  res.send("hello new user");
+});
+
+http.listen(PORT, function () {
+  console.log(`listening on port ${PORT}`);
+});
