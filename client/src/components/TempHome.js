459c042a8612a7a367b5950cd841afeca140ccae
flavio herrera <herreraflavio0@gmail.com>
2023-01-30 23:28:27 -0800

    removed button

--- client/src/components/TempHome.js
+++ client/src/components/TempHome.js
@@ -3,19 +3,6 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 function TempHome() {
   const status = localStorage.getItem("status");
-  const Image = () => {
-    axios({
-      method: "POST",
-      withCredentials: true,
-      url: `/image`,
-    }).then((res) => {
-      if (res.data.msg === "error") {
-        console.log("there was an error getting image");
-      } else {
-        console.log(res.data);
-      }
-    });
-  };
   return (
     <div>
       <h1>tempHome</h1>
@@ -27,7 +14,6 @@ function TempHome() {
           <button>
             <Link to="/lurker">open app</Link>
           </button>
-          <button onClick={Image}>get image url</button>
         </div>
       ) : (
         <div>
